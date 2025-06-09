f1c2841f934a3601108a2aaa1679dfa68bb71821
NUHASHROXME <nuhashroxme@gmail.com>
2025-06-09 22:58:42 +0600

    chore: 🌟 initaiated X-DOCS

---
+++ src/app/documents/[documentId]/page.tsx
@@ -0,0 +1,15 @@
+import { Editor } from "./editor";
+interface DocumentPageProps {
+    params: Promise<{ documentId: string }>;
+};
+
+const DocumentPage = async ({ params }: DocumentPageProps) => {
+    const { documentId } = await params;
+    return (
+        <div>
+            <h1>Document ID: {documentId}</h1>
+            <Editor />
+        </div>
+    );
+}
+export default DocumentPage;
---
+++ src/app/documents/page.tsx
@@ -0,0 +1,9 @@
+export default function DocumentsPage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-grey-700">
+      <h1 className="text-4xl font-bold mb-6">Documents</h1>
+      <p className="text-lg mb-4">Collaborative Docs Writing Platform</p>
+      <p className="text-sm text-gray-400">This is a placeholder for the documents page.</p>
+    </div>
+  );
+}
